9441624ee4d4db8ec99a6c38efecf907adcffd8f
Deepanshu Sharma <deesharm36@publicisgroupe.net>
2024-04-22 22:06:36 +0530

    intersation observers updated

--- src/component/nav/Nav.jsx
+++ src/component/nav/Nav.jsx
@@ -19,18 +19,18 @@ const Nav = () => {
 
  
           const options = { threshold: 0.5 };
-          const observer1 = new IntersectionObserver(callback, options);
+          const observer = new IntersectionObserver(callback, options);
          
           const target1 = document.querySelector("#home");
           const target2 = document.querySelector("#about");
           const target3 = document.querySelector("#contact");
           const target4 = document.querySelector("#portfolio");
           const target5 = document.querySelector("#experience");
-          observer1.observe(target1)
-          observer1.observe(target2)
-          observer1.observe(target3)
-          observer1.observe(target4)
-          observer1.observe(target5)
+          observer.observe(target1)
+          observer.observe(target2)
+          observer.observe(target3)
+          observer.observe(target4)
+          observer.observe(target5)
          
     })
     
@@ -39,7 +39,7 @@ const Nav = () => {
            <a onClick={()=>setActiveNav("#home")} href="#home" className={activeNav==="#home"?"active":""}><ToolTip title="home"><AiOutlineHome/></ToolTip></a> 
            <a onClick={()=>setActiveNav("#about")} href="#about" className={activeNav==="#about"?"active":""}><ToolTip title="about"><AiOutlineUser/></ToolTip></a> 
            <a onClick={()=>setActiveNav("#experience")}  href="#experience" className={activeNav==="#experience"?"active":""}><ToolTip title="experience"><BiBook/></ToolTip></a> 
-           <a onClick={()=>setActiveNav("#services")} href="#portfolio" className={activeNav==="#portfolio"?"active":""}><ToolTip title="portfolio"><RiServiceLine/></ToolTip></a> 
+           <a onClick={()=>setActiveNav("#portfolio")} href="#portfolio" className={activeNav==="#portfolio"?"active":""}><ToolTip title="portfolio"><RiServiceLine/></ToolTip></a> 
            <a onClick={()=>setActiveNav("#contact")} href="#contact" className={activeNav==="#contact"?"active":""}><ToolTip title="contact"><BiMessageSquareDots/></ToolTip></a> 
         </nav>
     );
